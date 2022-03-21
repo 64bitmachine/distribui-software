17cab30dc8afa3be14664b63ea2a708b44945153
Rathod <dinesh.rathod.c2019@iitbombay.org>
2022-03-21 16:06:05 +0530

    FullFill order requesting wallet and restaurant code implemented

--- Project 2/src/main/java/com/example/FulFillOrder.java
+++ Project 2/src/main/java/com/example/FulFillOrder.java
@@ -1,12 +1,17 @@
 package com.example;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.Map;
 
 import com.example.Agent.AgentCommand;
 import com.example.dto.DeliveryAgent;
+import com.example.dto.Item;
 import com.example.dto.OrderStatus;
 import com.example.dto.PlaceOrder;
-import com.typesafe.config.Config;
 import com.typesafe.config.ConfigFactory;
 
 import akka.actor.typed.ActorRef;
@@ -20,117 +25,199 @@ public class FulFillOrder extends AbstractBehavior<FulFillOrder.Command> {
     private Map<Integer, ActorRef<AgentCommand>> agentMap;
     private PlaceOrder placeOrder;
     private Integer orderId;
-    private String  orderStatus;
+    private String orderStatus;
     private Integer agentId;
+    private Item item;
     private ActorRef<Agent.AgentCommand> agentRef;
 
-    public static final String WALLET_SERVICE = ConfigFactory.load().getConfig("my-app.wallet-server").getString("address");
-    public static final String RESTAURANT_SERVICE = ConfigFactory.load().getConfig("my-app.restaurant-server").getString("address");
+    public static final String WALLET_SERVICE = ConfigFactory.load().getConfig("my-app.wallet-server")
+                                                                                .getString("address");
+    public static final String RESTAURANT_SERVICE = ConfigFactory.load().getConfig("my-app.restaurant-server")
+                                                                                .getString("address");
 
     public FulFillOrder(ActorContext<Command> context, PlaceOrder placeOrder,
-            Integer orderId,
-            Map<Integer, ActorRef<AgentCommand>> agentMap) {
-        
+                    Integer orderId, Map<Integer, ActorRef<AgentCommand>> agentMap) {
+
         super(context);
         getContext().getLog().info("Creating Order with order Id {}", orderId);
-
-        System.out.println("Wallet Service: " + WALLET_SERVICE);
-        System.out.println("Restaurant Service: " + RESTAURANT_SERVICE);
 
         this.placeOrder = placeOrder;
         this.agentMap = agentMap;
         this.orderId = orderId;
-        this.orderStatus = OrderStatus.unassigned; 
+        this.orderStatus = OrderStatus.unassigned;
         this.agentId = null;
         this.agentRef = null;
-        //@TODO Using hhtp client connect to wallet and restaurant
-        // placeOrder();
-
+        this.item = null;
         
+
+        // http post request to restaurant service to place order
+        try {
+            URL url = new URL(RESTAURANT_SERVICE + "/acceptOrder");
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+
+            // post request
+            conn.setRequestMethod("POST");
+            conn.setRequestProperty("Content-Type", "application/json");
+            conn.setRequestProperty("Accept", "application/json");
+            conn.setDoOutput(true);
+
+            String input = "{\"restId\": " + placeOrder.getRestId() + ", \"itemId\": " + placeOrder.getItemId()
+                    + ", \"qty\": " + placeOrder.getQty() + "}";
+
+            // send request
+            conn.getOutputStream().write(input.getBytes());
+            conn.getOutputStream().flush();
+
+            // get response code and response json object
+            if (conn.getResponseCode() == 201) {
+                InputStream inputStream = conn.getInputStream();
+                StringBuffer response = new StringBuffer();
+                int i;
+                while ((i = inputStream.read()) != -1) {
+                    response.append((char) i);
+                }
+
+                // parsing json object {"restId": num, "itemId": x, "qty": y}
+                String[] tokens = response.toString().split(",");
+                String[] restId = tokens[0].split(":");
+                String[] itemId = tokens[1].split(":");
+                String[] qty = tokens[2].split(":");
+
+                // remove trailing } from qty
+                String[] qtyTokens = qty[1].split("}");
+
+                this.item = new Item(Integer.parseInt(restId[1].trim()), Integer.parseInt(itemId[1].trim()),
+                        Integer.parseInt(qtyTokens[0].trim()));
+                getContext().getLog().info("restaurant service response: {}", response.toString());
+            } else {
+                getContext().getLog().info("Order not placed");
+            }
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        // if item is not null then send request to wallet service to deduct amount
+        if (this.item != null) {
+            try {
+                URL url = new URL(WALLET_SERVICE + "/deductBalance");
+                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+
+                // post request
+                conn.setRequestMethod("POST");
+                conn.setRequestProperty("Content-Type", "application/json");
+                conn.setRequestProperty("Accept", "application/json");
+                conn.setDoOutput(true);
+
+                String input = "{\"custId\": " + placeOrder.getCustId() + ", \"amount\": " + placeOrder.getQty() * item.getPrice() + "}";
+
+                // send request
+                conn.getOutputStream().write(input.getBytes());
+                conn.getOutputStream().flush();
+
+                // get response code and response json object
+                if (conn.getResponseCode() == 201) {
+                    getContext().getLog().info("Order placed");
+                } else {
+                    getContext().getLog().info("Order not placed");
+                }
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        // @TODO Using hhtp client connect to wallet and restaurant
+        // placeOrder();
 
         getContext().getLog().info("Order created with order Id {}, order status {}", orderId, orderStatus);
     }
 
-    interface Command {}
+    interface Command {
+    }
 
     public static class ActorStatus implements Command {
         private ActorRef<Agent.AgentCommand> agentReplyTo;
 
         private DeliveryAgent agent;
-        ActorStatus(ActorRef<Agent.AgentCommand> agentReplyTo, DeliveryAgent agent){
+
+        ActorStatus(ActorRef<Agent.AgentCommand> agentReplyTo, DeliveryAgent agent) {
             this.agentReplyTo = agentReplyTo;
             this.agent = agent;
         }
     }
-/**    public static class AgentAssignConfirm implements Command
-    {
-        private ActorRef<Agent.AgentCommand> agentReplyTo;
-        private DeliveryAgent agent;
-        private Boolean isAssigned;
-        AgentAssignConfirm(ActorRef<Agent.AgentCommand> agentReplyTo, DeliveryAgent agent, Boolean isAssigned ){
-            this.agentReplyTo = agentReplyTo;
-            this.agent = agent;
-            this.isAssigned = isAssigned;
-        }
-    }
-*/
-    public static class ActorStatusResponse{
-        
+
+    /**
+     * public static class AgentAssignConfirm implements Command
+     * {
+     * private ActorRef<Agent.AgentCommand> agentReplyTo;
+     * private DeliveryAgent agent;
+     * private Boolean isAssigned;
+     * AgentAssignConfirm(ActorRef<Agent.AgentCommand> agentReplyTo, DeliveryAgent
+     * agent, Boolean isAssigned ){
+     * this.agentReplyTo = agentReplyTo;
+     * this.agent = agent;
+     * this.isAssigned = isAssigned;
+     * }
+     * }
+     */
+    public static class ActorStatusResponse {
+
     }
 
     public static class OrderDelivered implements Command {
 
     }
 
-    public static Behavior<Command> create(PlaceOrder placeOrder, Integer orderId, Map<Integer, ActorRef<AgentCommand>> agentMap) {
+    public static Behavior<Command> create(PlaceOrder placeOrder, Integer orderId,
+            Map<Integer, ActorRef<AgentCommand>> agentMap) {
         return Behaviors.setup(context -> new FulFillOrder(context, placeOrder, orderId, agentMap));
     }
 
     @Override
     public Receive<Command> createReceive() {
         return newReceiveBuilder()
-        .onMessage(ActorStatus.class, this::onActorStatus)
-        //.onMessage(AgentAssignConfirm.class, this::onAgentAssignConfirm)
-        .onMessage(OrderDelivered.class, this::onOrderDelivered)
-        .build();
+                .onMessage(ActorStatus.class, this::onActorStatus)
+                // .onMessage(AgentAssignConfirm.class, this::onAgentAssignConfirm)
+                .onMessage(OrderDelivered.class, this::onOrderDelivered)
+                .build();
     }
 
-    private Behavior<Command> onActorStatus(ActorStatus actorStatus)
-    {
-        //TODO Implement onActorStatus
-        
-        /** If we are recieving ActorStatus Message, it simply means that
-         * actor is available while sending the ActorStatus Message to 
+    private Behavior<Command> onActorStatus(ActorStatus actorStatus) {
+        // TODO Implement onActorStatus
+
+        /**
+         * If we are recieving ActorStatus Message, it simply means that
+         * actor is available while sending the ActorStatus Message to
          * FulFill Order
          */
-        if(orderStatus.equals(OrderStatus.unassigned))
-        {
-            getContext().getLog().info("FulFillOrder : Setting agentId to {} for order Id {}",actorStatus.agent.getAgentId(), orderId);
-            actorStatus.agentReplyTo.tell(new Agent.ConfirmationRequest(orderId,true,getContext().getSelf()));
+        if (orderStatus.equals(OrderStatus.unassigned)) {
+            getContext().getLog().info("FulFillOrder : Setting agentId to {} for order Id {}",
+                    actorStatus.agent.getAgentId(), orderId);
+            actorStatus.agentReplyTo.tell(new Agent.ConfirmationRequest(orderId, true, getContext().getSelf()));
             agentId = actorStatus.agent.getAgentId();
             agentRef = actorStatus.agentReplyTo;
-        }
-        else{
-            actorStatus.agentReplyTo.tell(new Agent.ConfirmationRequest(orderId,false,getContext().getSelf()));
+        } else {
+            actorStatus.agentReplyTo.tell(new Agent.ConfirmationRequest(orderId, false, getContext().getSelf()));
         }
         return Behaviors.same();
     }
 
     // private Behavior<Command> onAgentAssignConfirm(AgentAssignConfirm message){
-    //     // ? For now I am resetting the value if agent fail to assign. 
-    //     //Need to discuss
-    //     if(message.agent.getAgentId() ==agentId && message.isAssigned == false)
-    //     {
-    //         agentId = null;
-    //         agentRef = null;
-    //     }
-    //     return this;
+    // // ? For now I am resetting the value if agent fail to assign.
+    // //Need to discuss
+    // if(message.agent.getAgentId() ==agentId && message.isAssigned == false)
+    // {
+    // agentId = null;
+    // agentRef = null;
+    // }
+    // return this;
     // }
 
-
-    private Behavior<Command> onOrderDelivered(OrderDelivered orderDelivered)
-    {
-        //TODO Implement OrderDelivered
+    private Behavior<Command> onOrderDelivered(OrderDelivered orderDelivered) {
+        // TODO Implement OrderDelivered
         return null;
     }
 
@@ -139,13 +226,12 @@ public class FulFillOrder extends AbstractBehavior<FulFillOrder.Command> {
         return false;
     }
 
-    private void probeAgents(){
+    private void probeAgents() {
         /** Will receive ActorStatus Message if agent is available */
-        for(Integer id : agentMap.keySet())
-        {
-            getContext().getLog().info("Sending AvailableRequest to {} from FulFillOrder for order Id {}",id, orderId);
+        for (Integer id : agentMap.keySet()) {
+            getContext().getLog().info("Sending AvailableRequest to {} from FulFillOrder for order Id {}", id, orderId);
 
-            agentMap.get(id).tell(new Agent.AvailableRequest(orderId,getContext().getSelf()));
+            agentMap.get(id).tell(new Agent.AvailableRequest(orderId, getContext().getSelf()));
         }
     }
 }
--- Project 2/test.sh
+++ Project 2/test.sh
@@ -1,4 +1,8 @@
 clear
+
+curl -X POST localhost:8080/reInitialize
+curl -X POST localhost:8082/reInitialize
+
 curl -X POST localhost:8081/agentSignIn -H 'Content-type:application/json'  -d '{"agentId": 201}'
 echo ""
 curl -X GET localhost:8081/agent/201
@@ -20,4 +24,8 @@ curl -X GET localhost:8081/agent/202
 curl -X GET localhost:8081/agent/203
 echo ""
 curl -X POST localhost:8081/requestOrder -H 'Content-type:application/json'  -d '{"custId": 301, "restId": 101, "itemId": 1, "qty": 1}'
+echo ""
+sleep 1
+curl -X GET localhost:8082/balance/301
+curl -X GET localhost:8082/balance/302
 echo ""
