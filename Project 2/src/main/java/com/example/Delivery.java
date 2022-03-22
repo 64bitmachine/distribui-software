478162c1a00ed507bea0aeba7b592e8e70dbc98f
Rathod <dinesh.rathod.c2019@iitbombay.org>
2022-03-22 19:54:42 +0530

    added tracking feature

--- Project 2/src/main/java/com/example/Delivery.java
+++ Project 2/src/main/java/com/example/Delivery.java
@@ -25,7 +25,7 @@ public class Delivery extends AbstractBehavior<Delivery.Command> {
 
     private final static Logger log = LoggerFactory.getLogger(Delivery.class);
     private TreeMap<Integer, ActorRef<AgentCommand>> agentMap;
-    private TreeMap<Integer, ActorRef<FulFillOrder.Command>> orderMap;
+    private TreeMap<Integer, TrackOrder> orderMap;
     private Integer orderId;
 
     // actor protocol
@@ -53,6 +53,14 @@ public class Delivery extends AbstractBehavior<Delivery.Command> {
     public final static class ReInitializeResponse {
         public ReInitializeResponse() {
             log.info("ReInitialize successful");
+        }
+    }
+
+    public final static class AgentAssigned implements Command {
+        public final Integer orderId;
+
+        public AgentAssigned(Integer orderId) {
+            this.orderId = orderId;
         }
     }
 
@@ -139,8 +147,8 @@ public class Delivery extends AbstractBehavior<Delivery.Command> {
 
     private Behavior<Command> onReInitialize(ReInitialize command) {
         orderId = 1000;
-        orderMap.forEach((k, v) -> {
-            getContext().stop(v);
+        orderMap.forEach((orderId, trackOrder) -> {
+            getContext().stop(trackOrder.getOrderRef());
         });
         orderMap.clear();
         agentMap.forEach((k, v) -> v.tell(new Agent.SignInOut(false)));
@@ -156,14 +164,33 @@ public class Delivery extends AbstractBehavior<Delivery.Command> {
                 .onMessage(AgentSignInOutCommand.class, this::onAgentSignInOut)
                 .onMessage(GetAgentCmd.class, this::onGetAgent)
                 .onMessage(GetOrderCmd.class, this::onGetOrder)
+                .onMessage(AgentAssigned.class, this::onAgentAssigned)
                 .build();
+    }
+
+    private Behavior<Command> onAgentAssigned(AgentAssigned agentAssigned) {
+        orderMap.get(agentAssigned.orderId).setIsAgentAssigned(true);
+        return Behaviors.same();
     }
 
     private Behavior<Command> onAgentSignInOut(AgentSignInOutCommand agentSignInCmd) {
         log.info("Agent with Id {} has signed in", agentSignInCmd.agentId);
         agentMap.get(agentSignInCmd.agentId).tell(new Agent.SignInOut(agentSignInCmd.isSignIn));
         agentSignInCmd.replyTo.tell(new AgentSignInOutResponse());
+        TrackOrder waitingOrder = getWaitingOrder();
+        if (waitingOrder != null) {
+            waitingOrder.getOrderRef().tell(new AssignThisAgent());
+        }
         return Behaviors.same();
+    }
+
+    private TrackOrder getWaitingOrder() {
+        for (Integer orderId : orderMap.keySet()) {
+            TrackOrder order = orderMap.get(orderId);
+            if (order.getIsAgentAssigned() == false)
+                return order;
+        }
+        return null;
     }
 
     /**
@@ -176,7 +203,7 @@ public class Delivery extends AbstractBehavior<Delivery.Command> {
         ActorRef<FulFillOrder.Command> orderActor = getContext().spawn(
                 FulFillOrder.create(reqOrder.placeOrder, orderId, agentMap),
                 "Order-" + orderId);
-        orderMap.put(orderId, orderActor);
+        orderMap.put(orderId, new TrackOrder(orderActor, false));
         reqOrder.replyTo.tell(new RequestOrderResponse(orderId));
         orderId++;
         return this;
@@ -189,7 +216,8 @@ public class Delivery extends AbstractBehavior<Delivery.Command> {
 
     private Behavior<Command> onGetOrder(GetOrderCmd getOrderCmd) {
         if (orderMap.containsKey(getOrderCmd.orderId)) {
-            orderMap.get(getOrderCmd.orderId).tell(new FulFillOrder.GetOrderCmd(getOrderCmd.replyTo));
+            ActorRef<FulFillOrder.Command> orderRef = orderMap.get(getOrderCmd.orderId).getOrderRef();
+            orderRef.tell(new FulFillOrder.GetOrderCmd(getOrderCmd.replyTo));
         } else {
             getContext().getLog().info("Order with Id {} not found", getOrderCmd.orderId);
             getOrderCmd.replyTo.tell(new FulFillOrder.GetOrderResponse(null));
---
+++ Project 2/src/main/java/com/example/TrackOrder.java
@@ -0,0 +1,36 @@
+package com.example;
+
+import akka.actor.typed.ActorRef;
+
+public class TrackOrder {
+    /**
+     * Track Order status and reference of the FulFillOrder Actor
+     * 
+     * @arg orderRef - ActorRef of FulFillOrder Actor
+     * @arg agentAssigned - Boolean variable which will be true is agent is assigned else false.
+     */
+    private ActorRef<FulFillOrder.Command> orderRef;
+    private boolean isAgentAssigned;
+    public TrackOrder(ActorRef<FulFillOrder.Command> orderRef, boolean isAgentAssigned)
+    {
+        this.orderRef = orderRef;
+        this.isAgentAssigned = isAgentAssigned;
+    }
+
+    public ActorRef<FulFillOrder.Command> getOrderRef() {
+        return this.orderRef;
+    }
+
+    public void setOrderRef(ActorRef<FulFillOrder.Command> orderRef) {
+        this.orderRef = orderRef;
+    }
+
+    public Boolean getIsAgentAssigned() {
+        return this.isAgentAssigned;
+    }
+
+    public void setIsAgentAssigned(Boolean agentAssigned) {
+        this.isAgentAssigned = agentAssigned;
+    }
+
+}
